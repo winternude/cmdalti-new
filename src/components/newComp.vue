fbf8709841684722d22fde3e3f4d33eace0085a5
Paragraph Design Studio <paragraph@10.0.0.2>
2022-06-17 17:34:07 +0200

    new

---
+++ src/components/newComp.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>this is an new card</p>
+  <h1>This is another Element</h1>
+</template>
+
+<script>
+export default {
+  name: "newComp",
+  props: {
+    text: String,
+  },
+};
+</script>
---
+++ src/views/PageOneView.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>New Element</h1>
+  <p>this is not mounted, why?</p>
+  <p>console.log</p>
+  <newComp />
+</template>
+
+<script>
+// @ is an alias to /src
+import newComp from "@/components/newComp.vue";
+
+export default {
+  name: "pageOne",
+  components: {
+    newComp,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
