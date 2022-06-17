a89a2762aa849e2c69b1a59601684a33370cbbe0
Paragraph Design Studio <paragraph@10.0.0.2>
2022-06-17 15:26:58 +0200

    message

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div><p>This is a Div Wow a new div</p></div>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Never Go Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
